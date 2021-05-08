1af0569766460ff032039b8a474873c573e3c52d
R Yang <ray_yang@mail.fresnostate.edu>
2021-05-07 21:44:02 -0700

    adding car model

--- dist/definitions/models/test.model.d.ts
+++ dist/definitions/models/test.model.d.ts
@@ -1,8 +1,9 @@
+/// <reference types="qs" />
 import { Request, Response, NextFunction } from 'express';
 export declare class Test {
     private norm;
     _model: any;
     constructor(norm: any);
     model: any;
-    testFunc: (model: any) => (req: Request, res: Response, next: NextFunction) => void;
+    testFunc: (model: any) => (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => void;
 }
