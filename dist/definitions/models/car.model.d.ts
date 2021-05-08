e87ce532ad72ddaa67e809f483cbe8ffcecd5324
R Yang <ray_yang@mail.fresnostate.edu>
2021-05-07 22:40:41 -0700

    part 3 video add

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -1,5 +1,9 @@
+/// <reference types="qs" />
+import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    getAllCars(model: any): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => Promise<void>;
+    getCarById(model: any): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => Promise<void>;
     model: any;
 }
