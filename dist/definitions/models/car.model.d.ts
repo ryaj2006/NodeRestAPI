36eeca0bc18f4ffc403f51632a6bc95a7f023ec3
R Yang <ray_yang@mail.fresnostate.edu>
2021-05-08 00:16:37 -0700

    finish car model endpoints

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -3,6 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    updateCar(model: any): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => Promise<void>;
+    createCar(model: any): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => Promise<void>;
     getAllCars(model: any): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => Promise<void>;
     getCarById(model: any): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) => Promise<void>;
     model: any;
--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,3 +1,4 @@
+import bodyParser = require('body-parser');
 import {Request, Response, NextFunction} from 'express';
 
 export class Car {
@@ -8,6 +9,7 @@ export class Car {
       make: { type: String, maxlength: 24 },
       model: { type: String, maxlength: 24 },
       year: { type: String, maxlength: 24 },
+      color: { type: String, maxlength: 24 },
       mileage: { type: String, maxlength: 24 },
       user_id: {
         type: Number,
@@ -23,15 +25,61 @@ export class Car {
         method: 'POST',
         callback: this.getAllCars,
         requireToken: true,
-      },
-      {
+      },{
         route: '/get-car-by-id/:id',
         method: 'POST',
         callback: this.getCarById,
         requireToken: true,
+      },{
+        route: '/create-car',
+        method: 'POST',
+        callback: this.createCar,
+        requireToken: true,
+      },{
+        route: '/update-car/id/:id',
+        method: 'PUT',
+        callback: this.updateCar,
+        requireToken: true,
+      },{
+        route: '/delete-car/id/:id',
+        method: 'DELETE',
+        callback: this.deleteCar,
+        requireToken: true,
       }
     ]];
   }
+
+
+  deleteCar(model: any) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        console.log('req.body===>', req.body);
+        let carCtrl = model.controller;
+        let resp = await carCtrl.remove(req, null, null);
+        res.json({ message: 'Success', resp });
+    }
+  }
+
+
+
+  updateCar(model: any) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        console.log('req.body===>', req.body);
+        let carCtrl = model.controller;
+        let resp = await carCtrl.update(req, null, null);
+        res.json({ message: 'Success', resp });
+    }
+  }
+
+
+  createCar(model: any) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        console.log('req.body===>', req.body);
+        let carCtrl = model.controller;
+        let resp = await carCtrl.insert(req, null, null);
+        res.json({ message: 'Success', resp });
+    }
+  }
+
 
   getAllCars(model: any) {
     return async (req: Request, res: Response, next: NextFunction) => {
